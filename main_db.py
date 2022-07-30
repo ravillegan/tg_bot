661091155fdae5bd7dacc1770293b96c3e64a4fb
r.r.ganiev <r.r.ganiev@macbook-r.r.ganiev>
2022-07-30 19:15:58 +0300

    kjh

--- main_db.py
+++ main_db.py
@@ -72,11 +72,12 @@ async def statistics(chat_id):
     # print(cursor, cursor1, 'blyat', str(chat_id), users_collection.count_documents({'chat_id': {'$eq': str(chat_id)}}))
     i = 0
     async for document in users_collection.find({'chat_id': str(chat_id)}).sort('score', -1):
-        stats_str += str(i+1)+ '. '+document.user_name
-        stats_str += ': '
-        stats_str += document.score
-        stats_str += '\n'
-        i+=1
+        print(document)
+        # stats_str += str(i+1)+ '. '+document.user_name
+        # stats_str += ': '
+        # stats_str += document.score
+        # stats_str += '\n'
+        # i+=1
     return stats_str
  
 async def user_info(user_info_json):
