6440e79cad7bd8f8869e3aa37ee5b82117d1695d
r.r.ganiev <r.r.ganiev@macbook-r.r.ganiev>
2022-07-30 20:14:32 +0300

    kjh

--- main_db.py
+++ main_db.py
@@ -131,7 +131,7 @@ async def start(message: types.Message):
         await bot.send_message(message.chat.id, 'Никто из вас еще не играет(( Хочешь играть - зарегайся /reg')
     else:
         user_id, new_old = await set_ochko_day(message.chat.id, user_id)
-        user_name = await users_collection.find({'chat_id': {'$eq': str(message.chat.id)}, 'user_id': {'$eq': str(user_id)}})
+        user_name = await users_collection.find_one({'chat_id': {'$eq': str(message.chat.id)}, 'user_id': {'$eq': str(user_id)}})
         user_name = user_name['user_name']
         if new_old == 'already':
             rand_reg = randint(0, 3)
