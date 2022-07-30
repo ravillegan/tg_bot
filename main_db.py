f6473206a424b671b18ca3f3a2cb2159ee96d1af
r.r.ganiev <r.r.ganiev@macbook-r.r.ganiev>
2022-07-30 19:53:51 +0300

    kjh

--- main_db.py
+++ main_db.py
@@ -53,10 +53,10 @@ async def set_ochko_day(chat_id, user_id):
     if await day_ochko_collection.count_documents({'chat_id': {'$eq': str(chat_id)}}) == 0:
         await day_ochko_collection.insert_one({'chat_id': str(chat_id), 'date': str(datetime.now().date()), 'user_name' : user_id})
         return user_id, 'new'
-    date_db = await users_collection.find_one({'chat_id': {'$eq': str(chat_id)}})
+    date_db = await day_ochko_collection.find_one({'chat_id': {'$eq': str(chat_id)}})
     print(date_db)
     if str(date_db['date']) == str(datetime.now().date()):
-        user_id_doc = users_collection.find({'date': {'$eq': str(datetime.now().date())}})
+        user_id_doc = day_ochko_collection.find({'date': {'$eq': str(datetime.now().date())}})
         user_id = user_id_doc['user_id']
         return user_id, 'already'
     await day_ochko_collection.update_one({'chat_id': str(chat_id), 'user_name' : user_id}, {'$set': {'date': str(datetime.now().date())}})
